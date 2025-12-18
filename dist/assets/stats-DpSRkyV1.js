def3f4a58a53462ebed0d0e15e6b66602ce56892
joshuaoliver <josh@bywave.com.au>
2025-12-18 23:29:09 +1100

    Update build artifacts and optimize asset files for improved performance and loading times. This includes various JavaScript and CSS files, ensuring the application runs more efficiently. Adjustments made to enhance the overall user experience and streamline the dashboard functionality.

--- dist/assets/stats-DpSRkyV1.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as o}from"./index-BiTP6UeF.js";function n(){return t.jsx(o,{})}export{n as component};
--- dist/assets/useSuspenseQuery-BI5dw9cZ.js
+++
@@ -1,1 +0,0 @@
-import{n as r,Q as u,o as s}from"./index-BiTP6UeF.js";function n(e,o){return r({...e,enabled:!0,suspense:!0,throwOnError:s,placeholderData:void 0},u)}export{n as u};
